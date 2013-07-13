abcf4939f54fae79cd95935bb8a6e3e11a200b5f
Rahul <rahul@rahul-VirtualBox.(none)>
2013-07-13 18:34:15 +0530

    Answer servlet

---
+++ src/main/java/browniepoints/servlet/AnswerServlet.java
@@ -0,0 +1,58 @@
+package main.java.browniepoints.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import main.java.browniepoints.model.CompositeQuestion;
+import main.java.browniepoints.model.helper.QuestionHelper;
+import main.java.browniepoints.util.Util;
+
+/**
+ * Servlet implementation class AnswerServlet
+ */
+public class AnswerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AnswerServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String qid = request.getParameter("qid");
+		String answer = request.getParameter("a");
+		
+		System.out.println(qid + ", " + answer);
+
+		if (Util.isNullOrEmpty(qid) || Util.isNullOrEmpty(answer)) {
+			request.setAttribute("response", null);
+			return;
+		}
+
+		CompositeQuestion ret = QuestionHelper.getInstance().evaluateAnswer(
+				Integer.parseInt(qid), answer);
+		Util.convertToJSON(ret, response);
+	}
+
+}
