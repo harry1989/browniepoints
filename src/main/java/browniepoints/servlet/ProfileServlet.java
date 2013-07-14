42db4395c4a8aac7c9c66bae9cd939450c14e19a
Rahul <rahul@rahul-VirtualBox.(none)>
2013-07-14 08:21:49 +0530

    profile

--- src/main/java/browniepoints/servlet/ProfileServlet.java
+++ src/main/java/browniepoints/servlet/ProfileServlet.java
@@ -1,14 +1,20 @@
 package main.java.browniepoints.servlet;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import main.java.browniepoints.model.CompositeQuestion;
 import main.java.browniepoints.model.User;
+import main.java.browniepoints.model.helper.QuestionHelper;
 import main.java.browniepoints.model.helper.UserHelper;
+import main.java.browniepoints.util.Util;
 
 /**
  * Servlet implementation class ProfileServlet
@@ -30,14 +36,20 @@ public class ProfileServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		Map<String, List<CompositeQuestion>> ret =
+				new HashMap<String, List<CompositeQuestion>>();
+		List<CompositeQuestion> lst = QuestionHelper.getInstance()
+				.getQuestionsForUser(UserHelper.getInstance().getLoggedInUid());
+
+		ret.put("my_q", lst);
+		Util.convertToJSON(ret, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void service(HttpServletRequest request,
+	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		Integer uid = UserHelper.getInstance().getLoggedInUid();
 		String email = UserHelper.getInstance().getUser(uid).getEmail();
@@ -45,8 +57,8 @@ public class ProfileServlet extends HttpServlet {
 		Object phone = request.getParameter("phone");
 
 		UserHelper.getInstance().update(
-				new User(uid, email, name == null ? "" : (String) name,
-						 0, phone == null ? "" : (String) phone));
+				new User(uid, email, name == null ? "" : (String) name, 0,
+						phone == null ? "" : (String) phone));
 	}
 
 }
